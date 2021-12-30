d6a906fbf384e6244bf24c7054afe4763ede06ed
Bruno Batista <bruno.batista@inventione.com.br>
2021-12-29 21:32:49 -0300

    My firts observable

--- RXJS/src/app/app.component.ts
+++ RXJS/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Subscriber } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -21,13 +22,35 @@ export class AppComponent implements OnInit{
     })
 
   }
+
+  minhaObservable(nome: string) : Observable<string>{
+    return new Observable(Subscriber =>{
+      if(nome === 'Eduardo'){
+        Subscriber.next("ola " + nome);
+        Subscriber.next('ola de novo ' + nome);
+        setTimeout(()=>{
+          Subscriber.next('resposta com daley ' + nome);
+        },5000);
+      }
+      else{
+        Subscriber.error('ops! deu erro.')
+      }
+     
+    });
+  }
   ngOnInit(): void {
     /* this.minhaPromisse("Eduardo")
     .then(result => console.log(result));
 */
+/*
     this.minhaPromisse("Lucas")
     .then(result => console.log(result))
     .catch(erro => console.log(erro))
+*/
+
+    this.minhaObservable('Eduardo')
+      .subscribe(result => console.log(result),
+      erro => console.log(erro))
   }
 
 }
